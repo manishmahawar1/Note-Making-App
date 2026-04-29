889b9a07b64383e6a34e6347280fc229be0b4e5d
Manish <khushikumari80005@gmal.com>
2026-04-29 17:32:50 +0530

    file name change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Home from "./pages/Home";
+import Login from "./Pages/Login.jsx";
+import Signup from "./Pages/Signup.jsx";
+import Home from "./Pages/Home.jsx";
 import { useContext } from "react";
 import { AuthContext } from "./Context/AuthContext.jsx";
 import { Toaster } from "react-hot-toast";
